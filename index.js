6581910a5c195848d05a1217d7455f46ee2aa37a
pothonprogramming <pothonprogramming@gmail.com>
2019-12-03 21:34:33 -0500

    Added styles and formatting

--- index.js
+++ index.js
@@ -20,9 +20,10 @@
       var example_element  = example_fragment.children[0];
 
       example_element.querySelector(".example-name").innerText = example_data.name;
-      example_element.querySelector(".example-code-link").href = "https://github.com/pothonprogramming/pothonprogramming.github.io/tree/master/content/" + example_data.path;
-      example_element.querySelector(".example-page-link").href = "content/" + example_data.path + "/" + example_data.page + ".html";
-      example_element.querySelector(".example-vlog-link").href = "https://www.youtube.com/watch?v=" + example_data.vlog;
+
+      example_element.querySelector('a[data-name="code"]').href = "https://github.com/pothonprogramming/pothonprogramming.github.io/tree/master/content/" + example_data.path;
+      example_element.querySelector('a[data-name="page"]').href = "content/" + example_data.path + "/" + example_data.page + ".html";
+      example_element.querySelector('a[data-name="vlog"]').href = "https://www.youtube.com/watch?v=" + example_data.vlog;
 
       html.example_container.appendChild(example_element);
 
