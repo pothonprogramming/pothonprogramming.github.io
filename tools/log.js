862d805e50c8a134ad3503bd62a7bea985c90def
Frank <frankarendpoth@gmail.com>
2020-12-30 19:13:01 -0500

    updates to site

--- tools/log.js
+++ tools/log.js
@@ -4,7 +4,3 @@ const Log = function(data) {
   this.element = document.createRange().createContextualFragment('<div class = "log"><span class = "log-date">' + data.date + '</span><span class = "log-name">' + data.name + '</span><p class = "log-note">' + data.note + '</p></div>').children[0];
 
 };
-
-Log.prototype = {
-
-};
--- tools/project.js
+++ tools/project.js
@@ -1,10 +1,8 @@
 const Project = function(data) {
 
+  var vlog = data.vlog ? '<a class = "project-link" href = "https://www.youtube.com/watch?v=' + data.vlog + '" target = "_blank">video</a>' : '';
+
   this.data    = data;
-  this.element = document.createRange().createContextualFragment('<div class = "project"><span class = "project-name">' + data.name + '</span><nav class = "project-links"><a class = "project-link" href = "https://github.com/pothonprogramming/pothonprogramming.github.io/tree/master/content/' + data.path + '" target = "_blank">code</a><a class = "project-link" href = "content/' + data.path + '/' + data.page + '.html' + '" target = "_blank">page</a><a class = "project-link" href = "https://www.youtube.com/watch?v=' + data.vlog + '" target = "_blank">vlog</a></nav><p class = "project-note">' + data.note + '</p></div>').children[0];
-
-};
-
-Project.prototype = {
+  this.element = document.createRange().createContextualFragment('<div class = "project"><span class = "project-name">' + data.name + '</span><nav class = "project-links"><a class = "project-link" href = "https://github.com/pothonprogramming/pothonprogramming.github.io/tree/master/content/' + data.path + '" target = "_blank">code</a><a class = "project-link" href = "content/' + data.path + '/' + data.page + '.html' + '" target = "_blank">page</a>' + vlog + '</nav><p class = "project-note">' + data.note + '</p></div>').children[0];
 
 };
